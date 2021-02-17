d47cb7d1b63224712e1225204a9f7a194cbe855a
marchuanv <marchuanv@gmail.com>
2021-02-17 17:31:12 +0200

    refactor

--- component.messagebus.publisher.js
+++ component.messagebus.publisher.js
@@ -1,53 +1,16 @@
 const utils = require("utils");
-const fs = require("fs");
 const delegate = require("component.delegate");
-const messageBusHost = require("component.messagebus.host");
-const requestHandlerSecure = require("component.request.handler.secure");
-const requestSecure = require("component.request.secure");
+const messageBusHostChannel = require("component.messagebus.host.channel");
 const logging = require("logging");
 logging.config.add("MessageBus Publisher");
 
 module.exports = { 
-    handle: async (callingModule, { publicHost, publicPort, privateHost, privatePort }) => {
-        const registerModule = `component.messagebus.publisher.register`;
-        delegate.register(registerModule, async ({ newHost, hosts }) => { //register host with publisher
-            let message = ""
-            const path = `/${newHost.channel}/publish`;
-            const publishModule = `component.messagebus.publisher.${newHost.channel}`;
-            delegate.register(publishModule, async ({ headers: { username, publishid, fromhost, fromport }, data }) => {
-                message = `publishing message to the ${newHost.channel} channel and notifying all hosts.`;
-                logging.write("MessageBus Publisher",message);
-                for(const remoteHost of hosts.filter(h => h.channel === newHost.channel)){
-                    await requestSecure.send({
-                        host: remoteHost.publicHost,
-                        port: remoteHost.publicPort,
-                        path: `/${remoteHost.channel}/publish`,
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                            username,
-                            hashedPassphrase: remoteHost.hashedPassphrase,
-                            hashedPassphraseSalt: remoteHost.hashedPassphraseSalt,
-                            fromhost,
-                            fromport,
-                            publishid
-                        }, 
-                        data
-                    });
-                };
-                await delegate.call(callingModule, { channel: newHost.channel });
-            });
-            message = `host is registered with publisher, messages can be published to ${newHost.publicHost}:${newHost.publicPort}/${path}`;
-            logging.write("MessageBus Publisher",message);
-            await requestHandlerSecure.handle(publishModule, { 
-                publicHost: newHost.publicHost,
-                publicPort: newHost.publicPort,
-                privateHost: newHost.privateHost,
-                privatePort: newHost.privatePort,
-                path
-            });
-            return { headers: { "Content-Type":"text/plain", "Content-Length": Buffer.byteLength(message) }, statusCode: 200, statusMessage: "Success", data: message };
+    handle: async (callingModule, { host, port }) => {
+        const registerModule = `component.messagebus.publisher`;
+        const name = `global`;
+        delegate.register(registerModule, name, async ({ newHost, hosts }) => { //register host with publisher
+          console.log("");
         });
-        await messageBusHost.handle(registerModule, { publicHost, publicPort, privateHost, privatePort });
+        await messageBusHostChannel.handle({ host, port });
     }
 };
--- test.js
+++ test.js
@@ -1,16 +1,71 @@
-const messageBusHost = require("./component.messagebus.publisher.js");
+const messageBusPublisher = require("./component.messagebus.publisher.js");
 const delegate = require("component.delegate");
+const request = require("component.request");
 (async() => { 
     const callingModule = "someapplicationinterestedinpublishevents";
     delegate.register(callingModule, () => {
         return { statusCode: 200, statusMessage: "Success", headers: {}, data: null };
     });
-    await messageBusHost.handle(callingModule, {
-        publicHost: "localhost", 
-        publicPort: 3000, 
-        privateHost: "localhost", 
-        privatePort: 3000
+    await messageBusPublisher.handle(callingModule, {
+        host: "localhost", 
+        port: 3000
     });
+
+    //Register New Host
+    const newHost = { host: "localhost", port: "6000" };
+    let results = await request.send({ 
+        host: "localhost",
+        port: 3000,
+        path: "/host",
+        method: "GET",
+        headers: { 
+            username: "marchuanv",
+            fromhost: "localhost",
+            fromport: 6000
+        }, 
+        data: JSON.stringify(newHost),
+        retryCount: 1
+    });
+    if (results.statusCode !== 200){
+        throw "New Request To Register New Host Test Failed";
+    }
+
+     //New Request To Secured Host To Register A Channel
+     results = await request.send({
+         host: newHost.host,
+         port: newHost.port,
+         path: "/channel/register",
+         method: "GET",
+         headers: { 
+             username: "marchuanv",
+             fromhost: "localhost",
+             fromport: 6000
+         }, 
+         data: `{ "channel":"apples" }`,
+         retryCount: 1
+     });
+     if (results.statusCode !== 200){
+         throw "New Request To Unsecured Host To Register A Channel Test Failed";
+     }
+
+    //New Publish Request To Unsecured Host Channel
+    results = await request.send({
+        host: newHost.host,
+        port: newHost.port,
+        path: "/apples/publish",
+        method: "POST",
+        headers: { 
+            username: "marchuanv",
+            fromhost: "localhost",
+            fromport: 6000
+        }, 
+        data: ``,
+        retryCount: 1
+    });
+    if (results.statusCode !== 200 && results.statusMessage !== "Publish Success"){
+        throw "New Publish Request To Unsecured Host Channel Test Failed";
+    }
+
 })().catch((err)=>{
     console.error(err);
 });
